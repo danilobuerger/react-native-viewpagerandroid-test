c5bacb76a42942bfffcb58e4f2301a26980dd81e
Danilo Bürger <info@danilobuerger.de>
2017-12-28 18:02:51 +0100

    Added View Pager Test

--- src/First.js
+++ src/First.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react"
-import { View, Button } from "react-native"
+import { View, Button, ViewPagerAndroid, Text } from "react-native"
+
+var styles = {
+  container: {
+    flex: 1
+  },
+  pageStyle: {
+    alignItems: "center",
+    padding: 20
+  }
+}
 
 export default class First extends Component {
   _showSecondScreen = () => {
@@ -7,8 +17,25 @@ export default class First extends Component {
   }
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <Text>
+          1. Scroll the View Pager. Notice that this scrolls smoothly.
+        </Text>
+        <Text>2. Tap the "Go To Second Screen"</Text>
+        <Text>3. Go Back</Text>
+        <Text>
+          4. Scroll the View Pager. Notice that this doesn't scroll smoothly
+          aynmore.
+        </Text>
         <Button onPress={this._showSecondScreen} title="Go To Second Screen" />
+        <ViewPagerAndroid style={styles.container} initialPage={0}>
+          <View style={styles.pageStyle} key="1">
+            <Text>First page (Scroll Me)</Text>
+          </View>
+          <View style={styles.pageStyle} key="2">
+            <Text>Second page (Scroll Me)</Text>
+          </View>
+        </ViewPagerAndroid>
       </View>
     )
   }
